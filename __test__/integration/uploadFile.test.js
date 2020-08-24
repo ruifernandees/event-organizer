747719f301836edf21fde9d73ac8e192f0e25614
ruifernandees <dev.ruifernandes@gmail.com>
2020-08-24 12:18:26 -0300

    Add uploadFile integration test

---
+++ __test__/integration/uploadFile.test.js
@@ -0,0 +1,21 @@
+import request from 'supertest';
+
+import app from '../../src/app';
+
+describe('File upload', () => {
+  it('should return status 200', async () => {
+    const response = await request(app)
+      .post('/event')
+      .attach('file', '__test__/attach/proposals.txt');
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should return status 500 when upload a non text/plain file', async () => {
+    const response = await request(app)
+      .post('/event')
+      .attach('file', '__test__/attach/image.png')
+
+      expect(response.status).toBe(500);
+  });
+});
