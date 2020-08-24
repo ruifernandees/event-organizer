8764db2a5ad72a9049df19ac0b28641c6142f853
ruifernandees <dev.ruifernandes@gmail.com>
2020-08-24 10:17:10 -0300

    Fix: server.js example route send method removed

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => { 
-  res.json({ message: "Hello World" }).send();
+  res.json({ message: "Hello World" }).status(200);
 });
 
 app.listen(3333);
