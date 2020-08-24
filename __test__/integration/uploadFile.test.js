1c1e74a70a8dd43f155295f3ad63f20921b41975
ruifernandees <dev.ruifernandes@gmail.com>
2020-08-24 19:09:39 -0300

    Rename event route to events

--- __test__/integration/uploadFile.test.js
+++ __test__/integration/uploadFile.test.js
@@ -5,7 +5,7 @@ import app from '../../src/app';
 describe('File upload', () => {
   it('should return status 200 when upload a text/plain file', async () => {
     const response = await request(app)
-      .post('/event')
+      .post('/events')
       .attach('file', '__test__/attach/proposals.txt');
 
     expect(response.status).toBe(200);
@@ -13,7 +13,7 @@ describe('File upload', () => {
 
   it('should return status 500 when upload a non text/plain file', async () => {
     const response = await request(app)
-      .post('/event')
+      .post('/events')
       .attach('file', '__test__/attach/image.png');
 
       expect(response.status).toBe(500);
@@ -21,7 +21,7 @@ describe('File upload', () => {
 
   it('should return status 400 when the file have some line with invalid format', async () => {
     const response = await request(app)
-      .post('/event')
+      .post('/events')
       .attach('file', '__test__/attach/proposals-wrong.txt');
 
     expect(response.status).toBe(400);
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import express, { Router } from 'express';
 import multer from 'multer';
 
 import multerConfig from './config/multer';
-import EventController from './controllers/EventController';
+import EventsController from './controllers/EventsController';
 
 const routes = express.Router();
 const upload = multer(multerConfig).single('file');
 
-const eventController = new EventController();
+const eventsController = new EventsController();
 
-routes.post('/event', upload, eventController.store);
+routes.post('/events', upload, eventsController.store);
 
 export default routes;
