5c4ef93d5aa41e16ef58e42982c5f0d743e7ff5e
ruifernandees <dev.ruifernandes@gmail.com>
2020-08-24 12:53:47 -0300

    Update title of it function of test uploadFile

--- __test__/integration/uploadFile.test.js
+++ __test__/integration/uploadFile.test.js
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from '../../src/app';
 
 describe('File upload', () => {
-  it('should return status 200', async () => {
+  it('should return status 200 when upload a text/plain file', async () => {
     const response = await request(app)
       .post('/event')
       .attach('file', '__test__/attach/proposals.txt');
@@ -14,7 +14,7 @@ describe('File upload', () => {
   it('should return status 500 when upload a non text/plain file', async () => {
     const response = await request(app)
       .post('/event')
-      .attach('file', '__test__/attach/image.png')
+      .attach('file', '__test__/attach/image.png');
 
       expect(response.status).toBe(500);
   });
