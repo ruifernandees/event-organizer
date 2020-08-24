e9e78294d1b0c0834a324d36d0b5e460d5d05494
ruifernandees <dev.ruifernandes@gmail.com>
2020-08-24 10:44:44 -0300

    Configure Multer for txt files

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => { 
+app.post('/file-post', multer(multerConfig).single('file'), (req, res) => { 
+  console.log(req.file);
+
   res.json({ message: "Hello World" }).status(200);
 });
 
